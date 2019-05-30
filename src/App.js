182af93f68ae61b983ba87c073ee3c6c11d40d24
ahmet <ahmet.kio@hotmail.com>
2019-05-30 12:04:00 +0100

    Ahmet-React-Project-Tvshows

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      episode_info: [],
+      imgSrc: null
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.tvmaze.com/shows/82/episodes`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          episode_info: data
+        });
+      });
+  }
+ 
+  render() {
+    return (
+      <div className="App-header App">
+        <h2>Game of Throne</h2>
+        {this.state.episode_info.map(episode => {
+          return (
+          <div>  
+            <h3>Episode Name: {episode.name}</h3>
+            <h4>Episode: {episode.number} - Season:{episode.season}</h4>
+            <img src={getImageForEpisode(episode)} alt="" />
+            <p>{episode.summary}</p>
+            <p>Airdate: {episode.airdate} </p> 
+            <br></br>
+          </div>  )
+        })}
+      </div>
+    );
+  }
 }
+
+function getImageForEpisode(ep) {
+  return ep.image ? ep.image.medium : "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+}
+
+/*
+       
+*/
 
 export default App;
